2e165ddbe07a0b3fd7b495983264f04aded2924a
Matt Brown <mattbrown7531@gmail.com>
2024-01-23 12:39:02 -0700

    Update urls.py

--- dynamic_forms/urls.py
+++ dynamic_forms/urls.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import url
+from django.urls import path, re_path
 
 from .views import data_set_detail
 
 urlpatterns = [
-    url(r'show/(?P<display_key>[a-zA-Z0-9]{24})/$', data_set_detail,
+    re_path(r'show/(?P<display_key>[a-zA-Z0-9]{24})/$', data_set_detail,
         name='data-set-detail'),
 ]
