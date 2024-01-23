5a71eb905cea1c193e6d8a03ddf7664424bcefb3
Matt Brown <mattbrown7531@gmail.com>
2024-01-23 13:18:59 -0700

    Update admin.py

--- dynamic_forms/admin.py
+++ dynamic_forms/admin.py
@@ -9,7 +9,7 @@ from django.forms.utils import flatatt
 from django.utils.encoding import force_text
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from dynamic_forms.formfields import formfield_registry
 from dynamic_forms.models import FormFieldModel, FormModel, FormModelData
