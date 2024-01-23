b5862e03c63bcf574caeda3e9177ac9750ce7f73
Matt Brown <mattbrown7531@gmail.com>
2024-01-23 12:47:50 -0700

    Update utils.py

--- dynamic_forms/utils.py
+++ dynamic_forms/utils.py
@@ -5,11 +5,11 @@ import types
 def is_old_style_action(func):
     if isinstance(func, types.FunctionType):
         # it's a regular function
-        argspec = inspect.getargspec(func)
+        argspec = inspect.getfullargspec(func)
         return len(argspec.args) == 2
     else:
         # it's class with __call__()
-        argspec = inspect.getargspec(func.__call__)
+        argspec = inspect.getfullargspec(func.__call__)
         return len(argspec.args) == 3  # mind the 'self' arg
 
 
